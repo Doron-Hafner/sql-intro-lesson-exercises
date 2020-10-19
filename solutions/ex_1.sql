ffda7c3ab3512641292a9408a3e3953a15aefd8f
Doron Hafner <dh9707@gmail.com>
2020-10-19 15:47:10 +0300

    finish exercises

--- solutions/ex_1.sql
+++ solutions/ex_1.sql
@@ -1,1 +1,23 @@
 USE sql_intro;
+-- CREATE TABLE Dolphin(
+--     name VARCHAR(20) NOT NULL PRIMARY KEY,
+--     color VARCHAR(15),
+--     height INT(10),
+--     healthy BOOLEAN
+-- )
+INSERT INTO Dolphin
+VALUES('Daron', 'Red', 1, 0);
+
+INSERT INTO Dolphin
+VALUES('Lebron', 'Yellow', 3, 1);
+
+INSERT INTO Dolphin
+VALUES('Ronald', 'Green', 2, 1);
+
+INSERT INTO Dolphin
+VALUES('Grisha', 'Blue', 1, 1);
+
+SELECT * FROM Dolphin
+WHERE height > 2;
+
+
--- solutions/ex_4.sql
+++ solutions/ex_4.sql
@@ -1,1 +1,5 @@
 USE sql_intro;
+UPDATE Dolphin 
+SET height = 10
+WHERE name = 'Daron';
+SELECT * FROM Dolphin;
